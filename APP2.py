92d52d214ea7878d57a536038375509efb61fc8f
najah-mereka <najah@mereka.io>
2026-03-18 11:39:11 +0800

    Update APP2.py

--- APP2.py
+++ APP2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
-#import plotly.express as px
+import plotly.express as px
 import plotly.graph_objects as go
 
 # --------------------------------------------------
